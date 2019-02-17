535d5c27444b9a39d9a399ffa1ad8be979ebf35c
rmcodestar <rmcodestar@naver.com>
2019-02-17 17:04:39 +0900

    [2019.02.16] add embed mongo db (spring data : h2 --> mongodb )

--- src/main/java/com/study/springcloud/model/Person.java
+++ src/main/java/com/study/springcloud/model/Person.java
@@ -2,35 +2,28 @@ package com.study.springcloud.model;
 
 import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-import javax.persistence.*;
 import java.time.LocalDateTime;
 
 /**
  * Created by rmcodestar on 2019. 2. 12..
  */
 @Data
-@Entity
-@Table(name = "person")
-@EntityListeners(AuditingEntityListener.class)
+@Document(collection = "person")
 public class Person {
     @Id
-    @GeneratedValue
     private Long id;
 
-    @Column(name = "name", nullable = false)
     private String name;
 
-    @Column(name = "age", nullable = false)
     private Integer age;
 
     @CreatedDate
-    @Column(name = "reg_ymdt")
     private LocalDateTime createdDateTime;
 
     @LastModifiedDate
-    @Column(name = "mod_ymdt")
     private LocalDateTime modifiedDateTime;
 }
