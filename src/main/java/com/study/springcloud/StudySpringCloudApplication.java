0694b1b5274fca3b1ebd4921676ef4fc41cb587e
rmcodestar <rmcodestar@naver.com>
2019-02-24 18:33:33 +0900

    [2019.02.15] spring-cloud-eureka-client example

--- src/main/java/com/study/springcloud/StudySpringCloudApplication.java
+++ src/main/java/com/study/springcloud/StudySpringCloudApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableSwagger2
 @EnableMongoAuditing
+@EnableDiscoveryClient
 @SpringBootApplication
 public class StudySpringCloudApplication {
 
